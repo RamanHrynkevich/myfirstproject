1d028d104313775a5799bd34210a306eeaef5790
Roman <grinkevich@gmail.com>
2021-02-08 16:02:57 +0300

    add words counter

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -7,4 +7,6 @@ def reverse(request):
 	user_name = request.GET['username']
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html',{'username':user_name,'usertext':user_text, 'reversedtext':reversed_text})
+	s_user_text = user_text.split()
+	len_user_text = len(s_user_text)
+	return render(request, 'reverse.html',{'username':user_name,'usertext':user_text, 'reversedtext':reversed_text, 'lenusertext':len_user_text})
